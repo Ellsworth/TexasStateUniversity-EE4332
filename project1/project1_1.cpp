10bda9dee2548a51b98c836be9bac91fc3cf7acb
Erich Ellsworth <kg5key@kg5key.com>
2022-02-03 16:01:54 -0600

    Convert to use vectors instead.

--- project1/project1_1.cpp
+++ project1/project1_1.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <vector>
 
 using namespace std;
 
-const int ROW = 3;
-const int COL = 3;
+const unsigned int ROW = 3, COL = 3;
 
-void mulMat(double mat1[][COL], double mat2[][COL], double result[ROW][COL]) {
-	//double result[ROW][COL];
+void mulMat(vector<double> mat1, vector<double> mat2, vector<double>& result) {
 
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
-			result[i][j] = 0;
+			result[(3 * i) + j] = 0;
 
 			for (int k = 0; k < ROW; k++) {
-				result[i][j] += mat1[i][k] * mat2[k][j];
+				result[(3 * i) + j] += mat1[(3 * i) + k] * mat2[(3 * k) + j];
 			}
             
 		}
@@ -23,13 +22,13 @@ void mulMat(double mat1[][COL], double mat2[][COL], double result[ROW][COL]) {
 	}
 }
 
-void printResult(double result[ROW][COL]) {
+void printResult(vector<double> result) {
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
 
 
-            if (result[i][j] > 0) cout << " ";
-			cout << result[i][j] << " ";
+            if (result[(3 * i) + j] > 0) cout << " ";
+			cout << result[(3 * i) + j] << " ";
             
 		}
 
@@ -37,32 +36,32 @@ void printResult(double result[ROW][COL]) {
 	}
 }
 
-int main(void) {
+int main() {
 
-	double matrix_x[ROW][COL] = {
-        { 02.33,   4.53, -98.40},
-        { 28.54, -75.60,  23.44},
-        {102.33,   3.30, -45.01},
+    vector<double> result(ROW * COL);
+
+    vector<double> matrix_x = {
+          2.33,   4.53, -98.40,
+         28.54, -75.60,  23.44,
+        102.33,   3.30, -45.01,
 	};
 
-	double matrix_y[ROW][COL] = {
-        { 46.730,   43.60,  92.1},
-        { -2.233, -116.80, -11.4},
-        {156.800,   13.44,  67.8},
+	vector<double> matrix_y = {
+         46.730,   43.60,  92.1,
+         -2.233, -116.80, -11.4,
+        156.800,   13.44,  67.8,
 	};
-
-	double result[ROW][COL];
 
 	chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
 	mulMat(matrix_x, matrix_y, result);
 
-	printResult(result);
-
 	chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+    printResult(result);
 
 	cout << "Time difference = " << chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "[µs]" << std::endl;
     cout << "Time difference = " << chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
 
-	return 0;
+
 }
