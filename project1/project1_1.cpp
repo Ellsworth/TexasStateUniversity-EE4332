bcb137f51045cc278618efa4174573710cae2d63
Erich Ellsworth <kg5key@kg5key.com>
2022-02-03 18:52:14 -0600

    Add required comments.

--- project1/project1_1.cpp
+++ project1/project1_1.cpp
@@ -1,18 +1,43 @@
+/******************************************************************************
+** Erich Scott Ellsworth, wqi7
+** EE4332 – MP#1
+** Filename: project1_1.cpp
+** Due: 2/7/22
+**
+** Objective:
+** Multiply two predefined matrices.
+**
+*******************************************************************************/
+
+// Includes.
 #include <iostream>
 #include <iomanip>
-#include <chrono>
-#include <vector>
+#include <chrono> // required for timing runtime.
+#include <vector> // semi-required depending on gcc version. For using vectors.
 
 using namespace std;
 
+// The size of the two matrices
 const unsigned int ROW = 3, COL = 3;
 
-void mulMat(vector<double> mat1, vector<double> mat2, vector<double>& result) {
+/***********************************************************
+** mulMat: Multiplies two matrices
+**
+** mat1 & mat2: vector matrices to multiply.
+** result: vector matrix of the result. Passed by reference
+**
+** https://en.wikipedia.org/wiki/Matrix_multiplication
+/***********************************************************/
 
+void mulMat(vector<double> mat1, vector<double> mat2, vector<double>& result) {
+	// Iterate row by row
 	for (int i = 0; i < ROW; i++) {
+
+		// For every row reset each value to zero. 
 		for (int j = 0; j < COL; j++) {
 			result[(3 * i) + j] = 0;
 
+			// Do the multiplication operation.
 			for (int k = 0; k < ROW; k++) {
 				result[(3 * i) + j] += mat1[(3 * i) + k] * mat2[(3 * k) + j];
 			}
@@ -22,7 +47,15 @@ void mulMat(vector<double> mat1, vector<double> mat2, vector<double>& result) {
 	}
 }
 
+/***********************************************************
+** printResult: Prints a matrix.
+**
+** result: vector matrix of the matrix to be printed.
+/***********************************************************/
+
 void printResult(vector<double> result) {
+
+	// Iterate for each row.
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
 
@@ -37,13 +70,17 @@ void printResult(vector<double> result) {
 }
 
 int main() {
-	
+
+	// Start the total runtime clock.	
 	chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
+	// Pipe stdout to output file.
 	freopen("project1_1.txt","w",stdout);
 
+	// Create and pre-allocate vector for the result.
     vector<double> result(ROW * COL);
 
+	// Matrices to multiply. Should _never_ be edited.
     vector<double> matrix_x = {
           2.33,   4.53, -98.40,
          28.54, -75.60,  23.44,
@@ -56,25 +93,29 @@ int main() {
         156.800,   13.44,  67.8,
 	};
 
+	// Requirement 1. Print out the two matrices.
 	printResult(matrix_x);
 	cout << endl;
 	printResult(matrix_y);
 	cout << endl;
 
+	// Start the multiplication clock.
 	chrono::steady_clock::time_point matrix_begin = chrono::steady_clock::now();
 
+	// Multiply the two matrices.
 	mulMat(matrix_x, matrix_y, result);
 
+	// Stop the multiplication clock.
 	chrono::steady_clock::time_point matrix_end = chrono::steady_clock::now();
 
     printResult(result);
 	cout << endl;
 
+	// Stop the total runtime clock.
 	chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
+	// Print timing data.
 	cout << "Total Program Runtime: " << chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "[µs]" << std::endl;	
 	cout << "Matrix Multiplication: " << chrono::duration_cast<std::chrono::microseconds> (matrix_end - matrix_begin).count() << "[µs]" << std::endl;
-    //cout << "Time difference = " << chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
-
 
 }
