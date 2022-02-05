297e1eff9eaeb12d96de524009b2744f321728a6
Erich Ellsworth <kg5key@kg5key.com>
2022-02-04 22:59:46 -0600

    Add some extra info to slurm jobs.

--- project1/project1_2.sh
+++ project1/project1_2.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -l
-	 
-#SBATCH -p shared 
-#SBATCH -n 1  
-#SBATCH -t 00:00:10    
+
+#SBATCH -p shared
+#SBATCH -n 1
+#SBATCH -t 00:00:10
 #SBATCH -J hpc-project1_2
+
+echo "Starting at `date`"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running on $SLURM_NPROCS processors."
+echo "Current working directory is `pwd`"
+
+echo ""
 
 module load gcc
 module list
 
+echo ""
+
 g++ -v
 
-cd /home/wqi7/TexasStateUniversity-EE4332/project1 
+echo ""
+
+cd /home/wqi7/TexasStateUniversity-EE4332/project1
 
 g++ project1_2.cpp -o project1_2
 
--- project1/project1_3.sh
+++ project1/project1_3.sh
@@ -1,16 +1,28 @@
 #!/bin/bash -l
-	 
-#SBATCH -p shared 
-#SBATCH -n 1  
+
+#SBATCH -p shared
+#SBATCH -n 1
 #SBATCH -t 06:00:00
 #SBATCH -J hpc-project1_3
+
+echo "Starting at `date`"
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running on $SLURM_NPROCS processors."
+echo "Current working directory is `pwd`"
+
+echo ""
 
 module load gcc
 module list
 
+echo ""
+
 g++ -v
 
-cd /home/wqi7/TexasStateUniversity-EE4332/project1 
+echo ""
+
+cd /home/wqi7/TexasStateUniversity-EE4332/project1
 
 g++ project1_3.cpp -o project1_3
 
