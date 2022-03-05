ad5ce045887aaa9fb48b0ea812d7d7e49cef73e6
Erich <kg5key@kg5key.com>
2022-03-05 01:57:23 -0600

    add required comments.

--- project3/project3_1.cpp
+++ project3/project3_1.cpp
@@ -2,14 +2,22 @@
 #include <iomanip>
 #include <chrono> // required for timing runtime.
 #include <vector> // semi-required depending on gcc version. For using vectors.
-#include <math.h>
+#include <math.h> // used for abs() function.
 
 using namespace std;
 
+// Global consts.
+
+// Total size for each axis of the matrix. eg. 1000 x 1000.
 const unsigned int SIZE = 1000;
+
+// Difference from the last run to consider an element in steady state.
 const float STEADY_STATE = 0.001;
 
+// The OFFSET of each value of the head applies at a given element.
 const float OFFSET = 100 / ((float)SIZE - 1);
+
+// Total elements of the plate.
 const int TOTAL_ELEMENTS = (SIZE - 2) * (SIZE - 2);
 
 
@@ -20,17 +28,24 @@ int countSteadyState(vector<float>& matrix1, vector<float>& matrix2);
 
 int main() {
 
+    // Define vectors
     vector<float> plate(SIZE * SIZE);
     vector<float> prev(SIZE * SIZE);
 
+    // Print the values of the consts defined at the begining.
     cout << "Offset is " << OFFSET << endl;
     cout << "Total elements of the plate is " << TOTAL_ELEMENTS << endl;
     cout << endl;
 
+    // Plate is filled with zeros, need to setup heat values.
     setupMatrixBorder(plate);
 
+    // While the number of steady state elements does not equal the total elements.
     do {
+        // Set the current plate values to the priv, so we can later compare.
         prev = plate;
+
+        // Do the Jacobi iteration.
         averageArray(plate);
     } while (countSteadyState(plate, prev) != TOTAL_ELEMENTS);
 
@@ -39,6 +54,12 @@ int main() {
 
 
 }
+
+/***********************************************************
+ * printMatrix - Prints a vector matrix.
+ * 
+ * matrix - the matrix to print
+/***********************************************************/
 
 void printMatrix(vector<float> matrix) {
 
@@ -54,6 +75,12 @@ void printMatrix(vector<float> matrix) {
 
 }
 
+/***********************************************************
+ * setupMatrixBorder - Sets up the heat values at the border off the matrix
+ * 
+ * matrix - the matrix to setup the border
+/***********************************************************/
+
 void setupMatrixBorder(vector<float>& matrix) {
 
     for (int row = SIZE; row > 0; row--) {
@@ -63,6 +90,12 @@ void setupMatrixBorder(vector<float>& matrix) {
     }
     
 }
+
+/***********************************************************
+ * averageArray - Perform the Jacobi iteration across the array
+ * 
+ * matrix - the matrix to perform the iteration
+/***********************************************************/
 
 void averageArray(vector<float>& matrix) {
 
@@ -75,6 +108,15 @@ void averageArray(vector<float>& matrix) {
         }
     }
 }
+
+/***********************************************************
+ * countSteadyState - Count the number of elements in steady state.
+ * 
+ * matrix1 - The first matrix to compare.
+ * matrix2 - The second matrix to compare.
+ * 
+ * returns: The total number of elements in steady state.
+/***********************************************************/
 
 int countSteadyState(vector<float>& matrix1, vector<float>& matrix2) {
 
