9aff9c1ffe2de6d52237742e5950bd9f197e698e
Erich <kg5key@kg5key.com>
2022-03-05 01:24:08 -0600

    Working project 3.1.

--- project3/project3_1.cpp
+++ project3/project3_1.cpp
@@ -2,25 +2,45 @@
 #include <iomanip>
 #include <chrono> // required for timing runtime.
 #include <vector> // semi-required depending on gcc version. For using vectors.
+#include <math.h>
 
 using namespace std;
 
-const unsigned int SIZE = 10 + 1;
-const short int offset = 100 / (SIZE - 1);
+const unsigned int SIZE = 4 + 1;
+const float STEADY_STATE = 0.001;
+
+const short int OFFSET = 100 / (SIZE - 1);
+const int TOTAL_ELEMENTS = (SIZE - 2) * (SIZE - 2);
+
 
 void printMatrix(vector<float> matrix);
 void setupMatrixBorder(vector<float>& matrix);
+void averageArray(vector<float>& matrix);
+int countSteadyState(vector<float>& matrix1, vector<float>& matrix2);
 
 int main() {
 
+    
+
     vector<float> plate(SIZE * SIZE);
+    vector<float> prev(SIZE * SIZE);
 
     //plate[(4 * SIZE) + 0] = 1;
 
-    cout << "Offset is " << offset << endl;
+    cout << "Offset is " << OFFSET << endl;
+    cout << "Total elements of the plate is " << TOTAL_ELEMENTS << endl;
+    cout << endl;
 
     setupMatrixBorder(plate);
+
+    do {
+        prev = plate;
+        averageArray(plate);
+    } while (countSteadyState(plate, prev) != TOTAL_ELEMENTS);
+
+    
     printMatrix(plate);
+
 
 }
 
@@ -42,11 +62,37 @@ void setupMatrixBorder(vector<float>& matrix) {
 
     for (int row = SIZE; row > 0; row--) {
 
-        matrix[row * SIZE] = offset * row;
-        matrix[(SIZE * (SIZE - 1)) + row] = 100 - (offset * row);
+        matrix[row * SIZE] = OFFSET * row;
+        matrix[(SIZE * (SIZE - 1)) + row] = 100 - (OFFSET * row);
+    }
+    
+}
 
+void averageArray(vector<float>& matrix) {
 
+    for (int row = 1; row < SIZE - 1; row++) {
+        for (int col = 1; col < SIZE - 1; col++) {
+
+            matrix[(row * SIZE) + col] = 0.25 * (matrix[((row + 1) * SIZE) + col] +
+            matrix[((row - 1) * SIZE) + col] + matrix[(row * SIZE) + col + 1] +
+            matrix[(row * SIZE) + col - 1]);
+        }
+    }
+}
+
+int countSteadyState(vector<float>& matrix1, vector<float>& matrix2) {
+
+    unsigned int totalSteadyState = 0;
+
+    for (int row = 1; row < SIZE - 1; row++) {
+        for (int col = 1; col < SIZE - 1; col++) {
+            if (abs(matrix1[(row * SIZE) + col] - matrix2[(row * SIZE) + col]) < STEADY_STATE)
+                totalSteadyState++;
+        }
     }
 
-    
+    cout << "Steady state: " << totalSteadyState << endl;
+
+    return totalSteadyState;
+
 }
