30a5fe66c670ac901c4f08519a7a8bf161e16c2b
Erich <kg5key@kg5key.com>
2022-03-06 18:16:53 -0600

    working project3.2

--- project3/project3_2.cpp
+++ project3/project3_2.cpp
@@ -11,7 +11,7 @@ using namespace std;
 // Global consts.
 
 // Total size for each axis of the matrix. eg. 1000 x 1000.
-const unsigned int SIZE = 5 + 2;
+const unsigned int SIZE = 7000;
 
 // Difference from the last run to consider an element in steady state.
 const float STEADY_STATE = 0.001;
@@ -31,65 +31,112 @@ void printMatrix(vector<float> matrix);
 void setupMatrixBorder(vector<float>& matrix);
 
 void averageArray(vector<float>& matrix);
-void averageRow(vector<float>& matrix, int row);
+void averageRow(vector<float>& matrix, int rowstart, int rowend);
 
-int countSteadyState(vector<float>& matrix1, vector<float>& matrix2);
+int countSteadyState(vector<float>& matrix1, vector<float>& matrix2, int rowstart, int rowend);
 
 
 int main(int argc, char *argv[]) {
 
 	int rank, numProcess, rc, tag, offset, rows;
+    int startrow, endrow;
 	double t1,t2;
   
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+
 	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcess);
 
-    cout << "Rank: " << rank << endl;
+    int steadystate, sum_steadystate;
+
+    //cout << "Rank: " << rank << endl;
 
     // Define vectors
     vector<float> plate(SIZE * SIZE);
     vector<float> prev(SIZE * SIZE);
 
+    // Calculate how many rows each process is responsible for. 
+    const int rowsPerProcess = (SIZE - 2) / (numProcess - 1);
+
+
+    /**
+     * Master process
+     */
     if (rank == 0) {
 
-
-        // Print the values of the consts defined at the begining.
-        cout << "Offset is " << OFFSET << endl;
-        cout << "Total elements of the plate is " << TOTAL_ELEMENTS << endl;
-        cout << endl;
-
+        // Setup the heat source on the matrix
         setupMatrixBorder(plate);
 
-        rows = SIZE / (numProcess - 1);
-		offset = 0;
-		tag = MASTER_ID;
+        // Print the values of the consts defined at the begining.
+        cout << "Rows pers process: " << rowsPerProcess << endl;
+        cout << endl;
 
-        //MPI_Send(&plate[(2 - 1) * SIZE], (3 * SIZE), MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
-        MPI_Send(&plate[(2 - 1) * SIZE], (3 * SIZE), MPI_FLOAT, 1, MASTER_ID, MPI_COMM_WORLD);
-        
-        //MPI_Recv(&plate[(2) * SIZE], (SIZE), MPI_FLOAT, 1, WORKER_ID, MPI_COMM_WORLD, &status);
+        do {
+            
+            sum_steadystate = 0;
 
-        //cout << "master:" << endl;
-        //printMatrix(plate);
+            prev = plate;
+
+            for (int process = 1; process < numProcess; process++) {
+
+                startrow = (process - 1) * rowsPerProcess + 1;
+                endrow = (process) * rowsPerProcess;
 
 
+                if (process == numProcess - 1) endrow = SIZE - 2;
+
+                cout << "Process " << rank << ": Process " << process << " will avg rows " << startrow
+                    << "-" << endrow << endl;
+
+                MPI_Send(&startrow, 1, MPI_INT, process, MASTER_ID, MPI_COMM_WORLD);
+                MPI_Send(&endrow, 1, MPI_INT, process, MASTER_ID, MPI_COMM_WORLD);
+
+                MPI_Send(&prev[startrow * SIZE], (SIZE * (endrow - startrow)), MPI_FLOAT, process, MASTER_ID, MPI_COMM_WORLD);
+
+
+                MPI_Send(&plate[(startrow - 1) * SIZE], (SIZE * (endrow - startrow + 2)), MPI_FLOAT, process, MASTER_ID, MPI_COMM_WORLD);
+                MPI_Recv(&plate[(startrow) * SIZE], (SIZE * (endrow - startrow + 1)), MPI_FLOAT, process, WORKER_ID, MPI_COMM_WORLD, &status);
+
+                MPI_Recv(&steadystate, 1, MPI_INT, process, WORKER_ID, MPI_COMM_WORLD, &status);
+                sum_steadystate += steadystate;
+                
+                cout << "Current steady state: " << sum_steadystate << endl;
+
+            }
+        } while(sum_steadystate < TOTAL_ELEMENTS - 1);
+
+        printMatrix(plate);
+        cout << endl;
 
     }
     if (rank > 0) {
 
-        //MPI_Recv(&plate[(2 - 1) * SIZE], (3 * SIZE), MPI_FLOAT, 1, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&plate[(2 - 1) * SIZE], (3 * SIZE), MPI_FLOAT, 0, MASTER_ID, MPI_COMM_WORLD, &status);
-        averageRow(plate, 2);
+        MPI_Recv(&startrow, 1, MPI_INT, 0, MASTER_ID, MPI_COMM_WORLD, &status);
+        MPI_Recv(&endrow, 1, MPI_INT, 0, MASTER_ID, MPI_COMM_WORLD, &status);
 
-        //MPI_Send(&plate[(2) * SIZE], (SIZE), MPI_FLOAT, 0, MASTER_ID, MPI_COMM_WORLD);
-        printMatrix(plate);
+        MPI_Recv(&prev[startrow * SIZE], (SIZE * (endrow - startrow)), MPI_FLOAT, 0, MASTER_ID, MPI_COMM_WORLD, &status);
+
+        MPI_Recv(&plate[(startrow - 1) * SIZE], (SIZE * (endrow - startrow + 2)), MPI_FLOAT, 0, MASTER_ID, MPI_COMM_WORLD, &status);
         
+        averageRow(plate, startrow, endrow);
+        
+        MPI_Send(&plate[startrow * SIZE], (SIZE * (endrow - startrow + 1)), MPI_FLOAT, 0, WORKER_ID, MPI_COMM_WORLD);
+
+        steadystate = countSteadyState(plate, prev, startrow, endrow);
+
+        MPI_Send(&steadystate, 1, MPI_INT, 0, WORKER_ID, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
+
+    if (rank == 0) {
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+    cout << "Total Program Runtime: " << chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "[µs]" << std::endl;
+    }
 }
 
 /***********************************************************
@@ -132,34 +179,21 @@ void setupMatrixBorder(vector<float>& matrix) {
  * averageArray - Perform the Jacobi iteration across the array
  * 
  * matrix - the matrix to perform the iteration
+ * rowstart - the upper bounds of iteration
+ * rowwend - the lower bounds of the iteration
 /***********************************************************/
 
-void averageArray(vector<float>& matrix) {
+void averageRow(vector<float>& matrix, int rowstart, int rowend) {
 
-    for (int row = 1; row < SIZE - 1; row++) {
+    for (int row = rowstart; row < rowend + 1; row++) {
         for (int col = 1; col < SIZE - 1; col++) {
 
-            matrix[(row * SIZE) + col] = 0.25 * (matrix[((row + 1) * SIZE) + col] +
-            matrix[((row - 1) * SIZE) + col] + matrix[(row * SIZE) + col + 1] +
-            matrix[(row * SIZE) + col - 1]);
+            matrix[(row  * SIZE) + col] = 0.25 * (matrix[((row + 1) * SIZE) + col] +
+                matrix[((row - 1) * SIZE) + col] + matrix[(row * SIZE) + col + 1] +
+                matrix[(row * SIZE) + col - 1]);
+
         }
     }
-}
-
-/***********************************************************
- * averageArray - Perform the Jacobi iteration across the array
- * 
- * matrix - the matrix to perform the iteration
-/***********************************************************/
-
-void averageRow(vector<float>& matrix, int row) {
-
-    for (int col = 1; col < SIZE - 1; col++) {
-        matrix[(row * SIZE) + col] = 0.25 * (matrix[((row + 1) * SIZE) + col] +
-        matrix[((row - 1) * SIZE) + col] + matrix[(row * SIZE) + col + 1] +
-        matrix[(row * SIZE) + col - 1]);
-    }
-
 }
 
 
@@ -172,11 +206,11 @@ void averageRow(vector<float>& matrix, int row) {
  * returns: The total number of elements in steady state.
 /***********************************************************/
 
-int countSteadyState(vector<float>& matrix1, vector<float>& matrix2) {
+int countSteadyState(vector<float>& matrix1, vector<float>& matrix2, int rowstart, int rowend) {
 
     unsigned int totalSteadyState = 0;
 
-    for (int row = 1; row < SIZE - 1; row++) {
+    for (int row = rowstart; row < rowend + 1; row++) {
         for (int col = 1; col < SIZE - 1; col++) {
 
             if ( abs(matrix1.at((row * SIZE) + col) - matrix1.at((row * SIZE) + col) < STEADY_STATE ) ) totalSteadyState++;
